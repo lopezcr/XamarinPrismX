2abea2f7080e5cd69ce78d67e50b6f7130ab4b74
rafa <fallo_kof@hotmail.com>
2020-01-06 11:53:55 -0600

    Clase notas

---
+++ XamarinPrismX/XamarinPrismX/ClaseNotas.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinPrismX
+{
+    class ClaseNotas
+    {
+
+    }
+}
+
+
+/*
+
+Esta clase la usare para agregar notas cuando vaya avanzando en los ejercicios
+y espero ir agregando notas durante cada commit
+
+//------------------------------------------------------------------------
+https://www.youtube.com/watch?v=81Q2fxFWIqA
+Prism for Xamarin.Forms - Create your first application
+
+
+-----
+*** Para agregar *** Xamarin prism en visual studio
+Menu-Extension-Manage Extensions
+Prism Template Pack
+-----
+
+Creamos el proyecto
+Prism Blank App
+
+
+ 
+     
+*/
